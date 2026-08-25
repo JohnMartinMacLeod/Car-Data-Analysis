0192e5390a0662a15aac2540e9c83d33a6c39e16
John (Martin) MacLeod <martinmacleod20@hotmail.com>
2026-08-25 15:28:57 +1000

    Update clean-dataset.py

--- clean-dataset.py
+++ clean-dataset.py
@@ -1,28 +1,28 @@
-#####################################################################################################
-# Some tasks in this assignment have been reorganised in order to support                           #
-# a more efficient and logical EDA pipeline. Below is a list of changes made to a tasks order.      #
-#                                                                                                   #
-# - Task 1c has been moved to be completed after 2b. This has been done to ensure that all          #
-# encoded columns have the same capitalisation, instead of having multiple encoded columns          #
-# that mean the same thing with different capitalisation.                                           #
-#                                                                                                   #
-# - Task 1e has been moved to be completed after 2b. This has been done to ensure that all          #
-# normalisation is completed after cleaning of outliers and missing values, ensuring the            #
-# normalised values will accurately reflect the data, rather than being compressed.                 #
-#                                                                                                   #
-# - Task 2a is initially completed before 2b to assist in exploring the uncleaned data and aid      #
-# in discovering any anomalies that may be present. Task 2a is completed again after completion     #
-# of 2b, as to demonstrate how unclean data can impact insights gained from the EDA process.        #
-#                                                                                                   #
-# Additionally, changes to a tasks order will be identified in a comment before the code block      #
-# pertaining to the task in question.                                                               #
-#####################################################################################################
+####################################################################################################
+# Some tasks in this assignment have been reorganised in order to support                          #
+# a more efficient and logical EDA pipeline. Below is a list of changes made to a tasks order.     #
+#                                                                                                  #
+# - Task 1c has been moved to be completed after 2b. This has been done to ensure that all         #
+# encoded columns have the same capitalisation, instead of having multiple encoded columns         #
+# that mean the same thing with different capitalisation.                                          #
+#                                                                                                  #
+# - Task 1e has been moved to be completed after 2b. This has been done to ensure that all         #
+# normalisation is completed after cleaning of outliers and missing values, ensuring the           #
+# normalised values will accurately reflect the data, rather than being compressed.                #
+#                                                                                                  #
+# - Task 2a is initially completed before 2b to assist in exploring the uncleaned data and aid     #
+# in discovering any anomalies that may be present. Task 2a is completed again after completion    #
+# of 2b, as to demonstrate how unclean data can impact insights gained from the EDA process.       #
+#                                                                                                  #
+# Additionally, changes to a tasks order will be identified in a comment before the code block     #
+# pertaining to the task in question.                                                              #
+####################################################################################################
 
 import pandas as pd
 
 RAW_DATA_PATH = "./car_dataset.csv"
-ENCODING = True
-NORMALISE = True
+ENCODING = False
+NORMALISE = False
 
 df = pd.read_csv(RAW_DATA_PATH)
 
@@ -115,7 +115,8 @@ def calculate_col_iqr(column : pd.Series):
 def get_outlier_mask(column : pd.Series, lower, upper):
     return((column > upper) | (column < lower))
 
-
+# Prints the name, lower and upper IQR fences, number of outliers, number of unique outliers,
+# and an numpy array of unique outliers for a specific column, column_name.
 def display_col_outlier_results(column_name : str, outliers_results : dict):
     print(
         f"\nColumn: {column_name}"
@@ -220,37 +221,55 @@ for column_name in COLS_TO_CHECK_FOR_OUTLIERS:
 ########################################
 
 display_col_outlier_results("mileage", outlier_results)
+df = df[(df["mileage"] <= 1000000)]
 
-# mileage - All outliers are above the upper fence, though the upper fence is a reasonable value for mileage on a vehicle.
-# While unlikely, it is possible for a vehicle to have a mileage of 1,000,000Km. Due to the improbability of this many 
-# vehicles having over 1,000,000Km mileage, all values over 1,000,000 are considered erroneous and have been removed.
+# mileage - The upper IQR fence is 150,000Km. While all outliers are above the upper IQR fence,
+# it is considered reasonable value for mileage on a vehicle. Some entries contain outlier 
+# values exceeding 1,000,000Km, which sounds erroneous. However, it is possible for 
+# a vehicle to have a mileage of 1,000,000Km. Though, it is statistically unlikely that
+# this dataset would contain this many vehicles that have over 1,000,000Km mileage.
+# Therefore, all values over 1,000,000 are considered erroneous and have been removed.
+
 
 display_col_outlier_results("num_of_doors", outlier_results)
+df = df[df["num_of_doors"].between(1, 7, inclusive = "both")]
 
-# num_of_doors - The lower fence is 2.5, but it is possible for a vehicle to have 1 door e.g., a bus. 
-# It is also possible, though uncommon, for a vehicle to have 7 doors. However, 42+ doors is clearly erroneous.
-# Therefore values between 1 and 7 were kept, due to being possible values, even if unlikely.
-# Values below 1 and above 7 have been removed due to their likely impossible nature. 
+# num_of_doors - The lower IQR fence is 2.5, but it is possible for a vehicle to 
+# have 1 door e.g., a bus. It is also possible, though uncommon, for a vehicle 
+# to have 7 doors. However, 42+ doors is clearly erroneous. Therefore values 
+# between 1 and 7 were kept, due to being possible values, even if unlikely. 
+# Values below 1 and above 7 are considered erroneous and have been removed.
+
 
 display_col_outlier_results("engine_capacity", outlier_results)
+df = df[~((df["engine_capacity"] == 0.2) & (df["type_of_engine"] == "petrol"))]
 
-# engine_capacity - The lower fence for engine capacity is 0.1L. 
-# However, an engine capacity of 0.1 or below is considered unlikely
-# for a non-electric vehicle. Therefore, any capacity 0.1 or lower 
-# has been removed. Manual inspection has revealed that one non-electric
-# vehicle is listed as having an engine capacity of 0.2L. This is much lower 
-# than the next closest non-electric vehicles engine capacity of 0.8L. 
-# As a result, the anomolous entry of 0.2 has also been removed.
-# The upper fence is 3.75L, however, many trucks have an engine capacity of 13 litres. 
-# As the highest valued outlier is 12.7 litres, values above the upper fence can remain.
+# engine_capacity - The lower IQR fence for engine capacity is 0.15L. 
+# Many electric vehicle entries have an engine capacity of 0.1L, however,
+# electric vehicles do not have an internal-combustion engine and therefore
+# do not have an engine capacity. These values associated with electric vehicles 
+# have been retained for now, and will instead be transformed in Task 2B ii.
+# Manual inspection revealed that one non-electric vehicle is listed as 
+# having an engine capacity of 0.2L. This is substantially lower than the other
+# non-electric vehicles represented in the dataset. Therefore, the value is 
+# considered erroneous, and has been removed.
+# The upper IQR fence is 3.75L. However, engine capacities above this value are 
+# possible for large vehicles, such as trucks. Manual inspection has revealed that
+# outliers above the upper IQR fence are associated with vehicles that could plausibly
+# have a large engine capacity, including the highest valued entry of 12.7 litres.
+# Therefore, values above the upper IQR fence have been retained.
 
 display_col_outlier_results("fuel_consumption", outlier_results)
+df = df[(df["fuel_consumption"] <= 20.0) | (df["fuel_consumption"].isna())]
 
-# fuel_consumption - some outliers above the upper fence are reasonable, while others verge on unlikely or impossible. 
+
+# fuel_consumption - Some outliers above the upper IQR fence are reasonable, while others verge on unlikely or impossible. 
 # The only value below the lower fence that could be considered unrealistic is 0, which is entirely achieveable in electric vehicles.
 # Therefore, values below the lower fence will remain, values above 20 will be removed.
 
 display_col_outlier_results("price_aud", outlier_results)
+df = df[df["price_aud"] >= 700]
+
 
 # price_aud - The IQR upper fence is $76,518.37, but this is a reasonable price for many vehices.
 # Manual inspection of outliers with values above the upper fence + standard deviation revealed that most vehicles
@@ -264,8 +283,11 @@ display_col_outlier_results("price_aud", outlier_results)
 ##############################################################
 # Justifications for missing and/or invalid values treatment #
 ##############################################################
-
 print(df.isna().sum())
+print("Missing fuel_system entries:", df["fuel_system"].isna().sum())
+# Fill empty "fuel_system" entries with "unknown".
+df[["fuel_system"]] = df[["fuel_system"]].fillna("unknown")
+print("Missing fuel_system entries:", df["fuel_system"].isna().sum())
 
 # fuel_system - Many of the fuel system values are missing. 
 # When purchasing a vehice, knowing the fuel system may be important.
@@ -274,11 +296,26 @@ print(df.isna().sum())
 # information to accurately impute a value with little-to-no doubt,
 # unknown values have been replaced with "unknown"
 
+print("Missing engine_capacity entries:", df["engine_capacity"].isna().sum())
+# Set fuel consumption and engine capacity values for electric vehicles to 0.0.
+
+#df["engine_capacity"] = df["engine_capacity"].where(df["type_of_engine"] != "electric", 0.0)
+
 # engine_capacity - Many of the engine capacity values are above 0
 # in entries where the type of engine is electric. Engine capacity represents 
 # the displacement of an internal combustion engine, measured in litres. 
 # Non-hybrid electric vehicles do not have an internal combustion engine and therefore 
 # have no engine displacement. As such, the engine capacity value of electric vehicles has been set to 0.
+
+print("Missing fuel_consumption entries:", df["fuel_consumption"].isna().sum())
+
+print("Number of entries where the fuel_consumption of an electric vehicle is not 0.0:",
+    ((df["type_of_engine"] == "electric") & ((df["fuel_consumption"] > 0.0) | df["fuel_consumption"].isna())).sum())
+
+df["fuel_consumption"] = df["fuel_consumption"].where(df["type_of_engine"] != "electric", 0.0)
+
+print("Number of entries where the fuel_consumption of an electric vehicle is not 0.0:",
+    ((df["type_of_engine"] == "electric") & ((df["fuel_consumption"] > 0.0) | df["fuel_consumption"].isna())).sum())
 
 # fuel_consumption - Similarly to engine capacity, many of the values in 
 # fuel consumption are above 0 in entries where the type of engine is electric.
@@ -286,9 +323,17 @@ print(df.isna().sum())
 # have a fuel consumption rate. As such, the fuel consumption value of electric vehicles
 # has been set to 0.
 
+print("Missing year_of_manufacture entries:", df["year_of_manufacture"].isna().sum())
+
 # year_of_manufacture - Manual inspection of the 32 missing values in year of manufacture
 # revealed that each of these entries has a note in the car_name column, indicating that 
 # the vehicle was manufactured "Before 1990". 
+
+print("Missing exterior_color entries:", df["exterior_color"].isna().sum())
+print("Missing interior_color entries:", df["interior_color"].isna().sum())
+# Clean the "exterior_color" and "interior_color" columns
+df[["exterior_color", "interior_color"]] = df[["exterior_color", "interior_color"]].replace(INVALID_COLOURS, "unknown")
+df[["exterior_color", "interior_color"]] = df[["exterior_color", "interior_color"]].replace("gray", "grey")
 
 # exterior_color and interior_color - These columns have both been given the same treatment,
 # as they are closely related and contain many overlapping values. Many values in these columns
@@ -297,20 +342,7 @@ print(df.isna().sum())
 # descriptive enough to convey useful information to someone reading the data, they have been replacedwith "unknown". 
 # Additionally, the American spelling of "gray" has been replaced with the English spelling, "grey".
 
-
-
-# Fill empty "fuel_system" entries with "unknown".
-df[["fuel_system"]] = df[["fuel_system"]].fillna("unknown")
-
-# Set fuel consumption and engine capacity values for electric vehicles to 0.0.
-df["fuel_consumption"] = df["fuel_consumption"].where(df["type_of_engine"] != "electric", 0.0)
-df["engine_capacity"] = df["engine_capacity"].where(df["type_of_engine"] != "electric", 0.0)
-
-# Clean the "exterior_color" and "interior_color" columns
-df[["exterior_color", "interior_color"]] = df[["exterior_color", "interior_color"]].replace(INVALID_COLOURS, "unknown")
-df[["exterior_color", "interior_color"]] = df[["exterior_color", "interior_color"]].replace("gray", "grey")
-
-
+print(df.isna().sum())
 ###############################################
 # Task 2A - Intentionally duplicated after 2B #
 ###############################################
